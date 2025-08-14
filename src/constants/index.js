8a79c79cc7c1e20562ed121bec5079c0cfdcdee9
hamza24v <hamzashueib23@gmail.com>
2025-08-14 09:32:34 -0400

    updated experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -101,7 +101,10 @@ const experiences = [
     date: "December 2024 - Present",
     points: [
       "Developed and maintained RESTful APIs using Spring Boot, implementing robust error handling and logging for high-traffic authentication endpoints",
-      "Demonstrated rapid codebase mastery by quickly adapting to the Java Spring Boot and Angular tech stack, earning recognition from management for immediate contributions to critical projects"
+      "Demonstrated rapid codebase mastery by quickly adapting to the Java Spring Boot and Angular tech stack, earning recognition from management for immediate contributions to critical projects",
+      "Spearheaded the development of an internal AI Productivity Hub with documentation and guides to boost team efficiency, including writing a “GitHub Copilot Model Selection Guide”, leading AI literacy efforts amid company-wide caution around AI adoption.",
+      "Recognized for rapid career advancement and AI expertise, earning selection to the CTO-led AI Task Force to strategically research, quantify, and communicate AI's transformative organizational value to executive leadership — a pivotal role in a highly risk-averse environment.",
+      "Championed enterprise-wide AI adoption by designing and delivering targeted training programs, and collaborating in live demonstrations and presentations to promote AI integration across departments.",
     ]
   },
   {
